eb5932df16479072d2414d6077f3bbcf448fa191
tduncan <tim@myorb.com>
2024-08-03 15:11:06 +0100

    remove unused import

--- apps/covertswarm/src/app/app.routes.ts
+++ apps/covertswarm/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Route } from '@angular/router';
-import { VulnerabilitiesDashboardComponent } from './features/vulnerabilities-dashboard/components/dashboard-container/vulnerabilities-dashboard.component';
 
 export const appRoutes: Route[] = [
   {
