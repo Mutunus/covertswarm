f2dc8e333f0a32dbb0b1c4b858b34d11eb5a3ac8
tduncan <tim@myorb.com>
2024-08-07 05:56:03 +0100

    add types to nodejs

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,19 +1,25 @@
 import express from 'express';
 import fetch from 'node-fetch';
+import {CisaVulnerabilities} from "@covertswarm/types";
+import {ErrorResponse} from "./types/types";
 
 const app = express();
 
-app.get('/api/cisa_vulnerabilities', async (req, res) => {
-  try {
-    const response = await fetch(
-      'https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json'
-    );
-    const data = await response.json();
-    res.json(data);
-  } catch (error) {
-    console.error('Error fetching vulnerabilities:', error);
-    res.status(500).send('An error occurred while fetching vulnerabilities');
-  }
+app.get('/api/cisa_vulnerabilities', async (req: express.Request, res: express.Response<CisaVulnerabilities | ErrorResponse>) => {
+  const response = await fetch(
+    'https://www.cisa.gov/sites/default/files/feeds/known_exploited_vulnerabilities.json'
+  );
+  const data = await response.json();
+  res.json(data);
+});
+
+app.use((err: any, req: express.Request, res: express.Response<ErrorResponse>) => {
+  const errorResponse: ErrorResponse = {
+    message: err.message,
+    statusCode: err.status || 500
+  };
+
+  res.status(errorResponse.statusCode).json(errorResponse);
 });
 
 const port = process.env.PORT || 3333;
